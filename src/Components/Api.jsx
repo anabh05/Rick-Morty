2fb63aea3e45b7374b55809c204f2dc9c99c98d4
anabh05 <anabh05@gmail.com>
2024-06-27 17:03:09 +0200

    logo

--- src/Components/Api.jsx
+++ src/Components/Api.jsx
@@ -11,7 +11,7 @@ export default function Api() {
     const paginacion = (pag) => {
 
         setPagina(pag) //( pagina-1 ) (pagina + 1) Modificar variable estado pagina
-        getCharacters(pag) //para indicar la pagina  los personajes por página
+        getCharacters(pag) //para indicar la pagina los personajes por página
 
     }
 
@@ -36,22 +36,39 @@ export default function Api() {
 
 
     return (
-        <>
-            <input type='text' onChange={(e) => filtrado(e)} className='form-data'></input>
-            {/* <button onClick={}>Buscar</button> */}
+
+    <>
+    
+    <div className='main'>
+                
+    <div className='logo'>
+        <img src='https://upload.wikimedia.org/wikipedia/fr/c/c8/Rick_and_Morty_logo.png'></img>
+    </div>
+                
+    <div className='search'>
+        <input type='text' onChange={(e) => filtrado(e)} className='form-data'></input>
+        {/* <button onClick={}>Buscar</button> */}
+    </div>
+
     <div className='characters'>
     {characters.map((character, index) => (
             
         <div className='character' key={index}>
             <img src={character.image}></img>
-            <p>{character.name}</p>
+            <div className='character-name'>
+                <p>{character.name}</p>
+            </div>
         </div>))}
           
             </div>
-            
+        
+    <div className='buttons'>
             <button onClick={() => { paginacion(pagina - 1) }}>Anterior</button>
-            <span>{pagina}</span>
-            <button onClick={() => { paginacion(pagina + 1) }}>Siguiente</button>
+            <span className='page'>{pagina}</span>
+                <button onClick={() => { paginacion(pagina + 1) }}>Siguiente</button>
+    </div>
+                
+    </div>
     </>
 
 
